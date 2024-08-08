fd09514fafdcec7f012449c3a209d0d91cc6a593
anacrislima <crisley.martins10@aluno.ifce.edu.br>
2024-08-08 15:06:31 -0300

    Alterado em 07/08/2024

--- avaliações/avaliação-03/exercicio-01/exercicio-01.c
+++ avaliações/avaliação-03/exercicio-01/exercicio-01.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  int x = 10;
- 
+  int x = 10; // valor da pontuação do jogador
+
+  if (x < 10) {
+    printf("Insuficiente\n");
+  } else if (x < 40) {
+    printf("Regular\n");
+  } else if (x < 70) {
+    printf("Bom\n");
+  } else if (x < 90) {
+    printf("Otimo\n");
+  } else {
+    printf("Excelente\n");
+  }
+
   return 0;
 }
